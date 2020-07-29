311ad52df63ffa6ac094bf2e43d9f9c92df660fc
agungmandala <agung30mandala@gmail.com>
2020-07-29 15:40:08 +0700

    record migrations

---
+++ packages/server/migrations/20200729081305-create-record.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => Promise.resolve()
+    .then(async () => {
+      await queryInterface.createTable('Record', {
+        id: {
+          type: Sequelize.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        type: {
+          type: Sequelize.TEXT,
+          defaultValue: 'expense',
+          allowNull: false,
+        },
+        category: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        amount: {
+          type: Sequelize.DECIMAL(12, 2),
+          allowNull: false,
+          defaultValue: 0,
+        },
+        transaction_date: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW,
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW,
+          allowNull: false,
+        },
+      })
+      await queryInterface.addIndex('Record', {
+        name: 'record_category_index',
+        fields: ['category'],
+      })
+    }),
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Record')
+  },
+}
