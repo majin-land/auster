b489bef84b08defdf86bfdfb2e5fc28b753d7daf
agungmandala <agung30mandala@gmail.com>
2020-08-06 13:30:09 +0700

    update record page ui

--- packages/site/src/pages/record/index.js
+++ packages/site/src/pages/record/index.js
@@ -11,15 +11,18 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
-  DialogContentText,
   DialogActions,
   FormControlLabel,
   RadioGroup,
   Radio,
   Tabs,
   Tab,
+  AppBar,
+  Divider,
+  Icons,
 } from '@material-ui/core'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import Lens from '@material-ui/icons/Lens'
 import { KeyboardDatePicker } from '@material-ui/pickers'
 
 import TabPanel from './tabpanel'
@@ -35,17 +38,15 @@ const Record = () => {
 
   const [open, setOpen] = useState(false)
   const [amount, setAmount] = useState(0)
+  const [category, setCategory] = useState(null)
   const [transactionDate, setTransactionDate] = useState(moment())
   const [note, setNote] = useState('')
-  const [activeTabIndex, setActiveTabIndex] = useState(0)
+  const [categoryTabIndex, setCategoryTabIndex] = useState(0)
+  const [transactionTabIndex, setTransactionTabIndex] = useState(1)
 
   const renderCurrentBalance = () => {
     return (
-      <div style={{
-        border: '1px black solid',
-        padding: '0.5rem',
-        borderRadius: '5px',
-      }}>
+      <div className={classes.currentBalance}>
         <div style={{
             fontWeight: 'bold'
           }}
@@ -109,10 +110,11 @@ const Record = () => {
           <FormControl fullWidth style={{ marginBottom: '1rem' }}>
             <TextField
               onClick={() => setOpen(true)}
-              label="Select Category"
+              placeholder="Select Category"
               type="text"
               disabled
               variant="outlined"
+              value={category}
               InputProps={{
                 endAdornment:
                   <InputAdornment position="end">
@@ -185,9 +187,7 @@ const Record = () => {
       >
         <DialogTitle id="alert-dialog-title">Select Category</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {renderCategoryTabPanel()}
-          </DialogContentText>
+          {renderCategoryTabPanel()}
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setOpen(false)} color="primary">
@@ -209,85 +209,233 @@ const Record = () => {
   }
 
   const renderCategoryTabPanel = () => {
-    <div>
+    return (
       <div>
-        <Tabs
-          variant="fullWidth"
-          textColor="primary"
-          indicatorColor="primary"
-          value={activeTabIndex}
-          onChange={(newIndex) => setActiveTabIndex(newIndex)}
-          aria-label="simple tabs example"
-        >
-          <Tab label={'EXPENSE'} {...a11yProps(0)} />
-          <Tab label={'INCOME'} {...a11yProps(1)} />
-        </Tabs>
+        <AppBar position="static">
+          <Tabs value={categoryTabIndex} onChange={(event, newIndex) => setCategoryTabIndex(newIndex)} aria-label="simple tabs example">
+            <Tab label="EXPENSE" {...a11yProps(0)} />
+            <Tab label="INCOME" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={categoryTabIndex} index={0}>
+          <FormControl component="fieldset">
+            <RadioGroup aria-label="expense" name="expense" value={category} onChange={(event, newValue) => setCategory(newValue)}>
+              <FormControlLabel value="Electricity" control={<Radio />} label="Electricity" />
+              <FormControlLabel value="Gas" control={<Radio />} label="Gas" />
+              <FormControlLabel value="Internet" control={<Radio />} label="Internet" />
+              <FormControlLabel value="Phone" control={<Radio />} label="Phone" />
+            </RadioGroup>
+          </FormControl>
+        </TabPanel>
+        <TabPanel value={categoryTabIndex} index={1}>
+          <FormControl component="fieldset">
+            <RadioGroup aria-label="income" name="income" value={category} onChange={(event, newValue) => setCategory(newValue)}>
+              <FormControlLabel value="Sallary" control={<Radio />} label="Sallary" />
+              <FormControlLabel value="Gift" control={<Radio />} label="Gift" />
+              <FormControlLabel value="Award" control={<Radio />} label="Award" />
+              <FormControlLabel value="Selling" control={<Radio />} label="Selling" />
+            </RadioGroup>
+          </FormControl>
+        </TabPanel>
       </div>
-      <TabPanel value={activeTabIndex} index={0}>
-        <FormControl component="fieldset">
-          <RadioGroup name="expense" onChange={() => {}}>
-            <FormControlLabel value="electricity" control={<Radio />} label="electricity" />
-            <FormControlLabel value="internet" control={<Radio />} label="internet" />
-          </RadioGroup>
-        </FormControl>
-      </TabPanel>
-      <TabPanel value={activeTabIndex} index={1}>
-        <FormControl component="fieldset">
-          <RadioGroup name="income" onChange={() => {}}>
-            <FormControlLabel value="electricity" control={<Radio />} label="electricity" />
-            <FormControlLabel value="internet" control={<Radio />} label="internet" />
-          </RadioGroup>
-        </FormControl>
-        <div style={{ padding: '1rem', textAlign: 'right' }}>
-          <Button>
-            OK
-          </Button>
+    )
+  }
+
+  const renderTransactionHistory = () => {
+    return (
+      <div>
+        <div style={{
+            fontWeight: 'bold',
+            marginBottom: '0.5rem',
+          }}>
+          Transaction History
         </div>
-      </TabPanel>
-    </div>
+        <div>
+          <AppBar position="static" color="default">
+            <Tabs
+              value={transactionTabIndex}
+              variant="fullWidth"
+              onChange={(event, newIndex) => setTransactionTabIndex(newIndex)}
+              aria-label="simple tabs example"
+            >
+              <Tab label="LAST MONTH" {...a11yProps(0)} />
+              <Tab label="THIS MONTH" {...a11yProps(1)} />
+              <Tab label="FUTURE" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={transactionTabIndex} index={0}>
+            <div>
+              <div>
+                29 July 2020
+              </div>
+              <Divider />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}
+              >
+                <Lens/>
+                <div
+                  style={{
+                    flex: 1
+                  }}
+                >
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      fontSize: '1rem',
+                    }}
+                  >
+                    <span>Electricity</span>
+                    <span>Rp 3.000.000</span>
+                  </div>
+                  <div
+                    style={{
+                      fontSize: '0.8rem'
+                    }}
+                  >
+                    ini adalah note untuk outcome
+                  </div>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel value={transactionTabIndex} index={1}>
+            <div>
+              <div>
+                29 Agustus 2020
+              </div>
+              <Divider />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}
+              >
+                <Lens/>
+                <div
+                  style={{
+                    flex: 1
+                  }}
+                >
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      fontSize: '1rem',
+                    }}
+                  >
+                    <span>Electricity</span>
+                    <span>Rp 3.000.000</span>
+                  </div>
+                  <div
+                    style={{
+                      fontSize: '0.8rem'
+                    }}
+                  >
+                    ini adalah note untuk outcome
+                  </div>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel value={transactionTabIndex} index={2}>
+            <div>
+              <div>
+                1 September 2020
+              </div>
+              <Divider />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}
+              >
+                <Lens/>
+                <div
+                  style={{
+                    flex: 1
+                  }}
+                >
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      fontSize: '1rem',
+                    }}
+                  >
+                    <span>Electricity</span>
+                    <span>Rp 3.000.000</span>
+                  </div>
+                  <div
+                    style={{
+                      fontSize: '0.8rem'
+                    }}
+                  >
+                    ini adalah note untuk outcome
+                  </div>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+        </div>
+      </div>
+    )
   }
 
   return (
-    <div className={classes.container}>
-      <div
-        style={{
-          display: 'flex',
-          flex: '1',
-          justifyContent: 'flex-end',
-          marginBottom: '0.5rem'
-        }}
-      >
-        <LogoutButton />
-      </div>
-      <div style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div style={{
-          flex: 1,
-          marginRight: '0.5rem',
-        }}>
-          {renderCurrentBalance()}
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <div className={classes.container}>
+        <div
+          style={{
+            display: 'flex',
+            flex: '1',
+            justifyContent: 'flex-end',
+            marginBottom: '0.5rem'
+          }}
+        >
+          <LogoutButton />
         </div>
         <div style={{
-          flex: 1,
-          border: '1px black solid',
-          padding: '0.5rem',
-          borderRadius: '5px',
-        }}>
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
           <div style={{
-                fontWeight: 'bold',
-                marginBottom: '0.5rem',
-            }}
-          >
-            Add Transaction
+            flex: 1,
+            marginRight: '0.5rem',
+          }}>
+            {renderCurrentBalance()}
+            {renderTransactionHistory()}
           </div>
-          {renderTransactionForm()}
+          <div style={{
+            flex: 1,
+            border: '1px black solid',
+            padding: '0.5rem',
+            borderRadius: '5px',
+          }}>
+            <div style={{
+                  fontWeight: 'bold',
+                  marginBottom: '0.5rem',
+              }}
+            >
+              Add Transaction
+            </div>
+            {renderTransactionForm()}
+          </div>
         </div>
+        {categoryDialog()}
       </div>
-      {categoryDialog()}
     </div>
   )
 }
--- packages/site/src/pages/record/styles.js
+++ packages/site/src/pages/record/styles.js
@@ -3,4 +3,11 @@ export default (theme) => ({
     maxWidth: theme.global.maxContentWidth,
     padding: '0.5rem',
   },
+  currentBalance: {
+    padding: '0.5rem',
+    borderRadius: '5px',
+    backgroundColor: theme.color.primary,
+    color: 'white',
+    marginBottom: '0.5rem',
+  },
 })
